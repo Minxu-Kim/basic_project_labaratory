05f2518398cd64bdf731041a84608cd42b02d8b0
myeong <jinmyeong2@naver.com>
2022-05-16 20:34:26 +0900

    commit

---
+++ app/src/main/java/com/example/basic_project_labaratory/Mission1Activity.java
@@ -0,0 +1,14 @@
+package com.example.basic_project_labaratory;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mission1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
