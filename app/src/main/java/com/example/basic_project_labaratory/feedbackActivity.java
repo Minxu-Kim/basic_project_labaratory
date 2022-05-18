22d759d4acfdb3f5fdc3c9b0bf6f6911217f1cc7
hjphan1030 <hjphan1030@gmail.com>
2022-05-18 15:30:26 +0900

    18조 화이팅

--- app/src/main/java/com/example/basic_project_labaratory/feedbackActivity.java
+++ app/src/main/java/com/example/basic_project_labaratory/feedbackActivity.java
@@ -1,6 +1,9 @@
 package com.example.basic_project_labaratory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
