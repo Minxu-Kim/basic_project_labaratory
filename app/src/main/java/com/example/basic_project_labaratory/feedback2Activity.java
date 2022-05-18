e53c7641485bb81408c2e40725032328bdcaba36
hjphan1030 <hjphan1030@gmail.com>
2022-05-18 18:28:41 +0900

    18조 화이팅

---
+++ app/src/main/java/com/example/basic_project_labaratory/feedback2Activity.java
@@ -0,0 +1,13 @@
+package com.example.basic_project_labaratory;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class feedback2Activity extends AppCompatActivity {
+    @Override // 식단 피드백 2 - 일별 식단 피드백
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback2);
+    }
+}
--- app/src/main/java/com/example/basic_project_labaratory/feedback3Activity.java
+++ app/src/main/java/com/example/basic_project_labaratory/feedback3Activity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class feedbackActivity extends AppCompatActivity {
-    @Override // 식단 피드백 1 - 식단 피드백 메인페이지 (기간별)
+public class feedback3Activity extends AppCompatActivity {
+    @Override // 식단 피드백 3 - 식사 상세정보
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback);
+        setContentView(R.layout.activity_feedback3);
     }
 }
