d3d0fc19eb5b0c12ef4642f6e712861d9e67f73f
hjphan1030 <hjphan1030@gmail.com>
2022-05-18 19:01:19 +0900

    18조 화이팅

--- app/src/main/java/com/example/basic_project_labaratory/feedback3Activity.java
+++ app/src/main/java/com/example/basic_project_labaratory/feedback3Activity.java
@@ -1,6 +1,9 @@
 package com.example.basic_project_labaratory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,16 @@ public class feedback3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback3);
+
+        Button toMain = findViewById(R.id.toMainFromFeedback3);
+
+        //랭킹 화면으로 넘어가기
+        toMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), homeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
